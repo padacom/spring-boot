0805d777aaaa05b0e9876cfea63ce7d3c01d6d6c
luzhigen <123456>
2019-09-16 21:59:38 +0800

    加入mq，启动监听

--- src/main/java/com/zglu/springboot/config/MQRunner.java
+++ src/main/java/com/zglu/springboot/config/MQRunner.java
@@ -8,6 +8,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Log
@@ -24,10 +25,13 @@ public class MQRunner implements ApplicationRunner {
         while (true) {
             try {
                 List<Message> messageList = mqConsumer.consumeMessage(3, 3);
+                List<String> handles = new ArrayList<>();
                 if (messageList != null) {
                     messageList.forEach(m -> {
                         log.info(m.toString());
+                        handles.add(m.getReceiptHandle());
                     });
+                    mqConsumer.ackMessage(handles);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
